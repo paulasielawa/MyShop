920191a29966f0bec9b650f8cd9c95ea4aa5b7c5
Paula Sielawa <paula@oivi.co>
2023-09-14 23:15:21 +0200

    advanced functionality for CRUD

---
+++ MyShop/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyShop.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ItemDbContext _itemDbContext;
+
+        public CustomerController(ItemDbContext itemDbContext)
+        {
+            _itemDbContext = itemDbContext;
+        }
+        
+        public async Task<IActionResult> Table()
+        {
+            List<Customer> customers = await _itemDbContext.Customers.ToListAsync();
+            return View(customers);
+        }
+    }
+}
+
---
+++ MyShop/ViewModels/CreateOrderItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MyShop.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+namespace MyShop.ViewModels;
+
+public class CreateOrderItemViewModel
+{
+	public OrderItem OrderItem { get; set; } = default!;
+	public List<SelectListItem> ItemSelectList { get; set; } = default!;
+	public List<SelectListItem> OrderSelectList { get; set; } = default!;
+	
+}
+
